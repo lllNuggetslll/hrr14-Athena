d5a256a5a485ae906d8f2ecbd89847ddc01bc695
David Nguyen <davidnguyen21@gmail.com>
2016-04-08 08:25:21 +0900

    added some dependencies (#16)

--- app/server.js
+++ app/server.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var morgan = require('morgan');
+var parser = require('body-parser');
 
 var app = express();
 
+app.use(morgan('dev'));
+app.use(parser.json());
 app.use(express.static(__dirname + '/../public'));
 
 
--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var app = require('./app/server.js');
 var port = process.env.PORT || 8000;
 
 
+
 app.listen(port, function(){
 console.log("Activate interlock! Dynotherms connected! Infracells up! Mega thrusters are go! LET'S GO VOLTRON FORCE!");
 });
+
 
 
 
