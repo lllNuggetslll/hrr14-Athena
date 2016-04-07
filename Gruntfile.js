7dd05eb0a2f7d7dd47662eac90ab8fdfd3daee9d
Ben Janes <brjanes@gmail.com>
2016-04-07 17:22:28 -0400

    (chore) fill out grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,73 @@ module.exports = function(grunt) {
   var jsApp = [];
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'public/dist/styles/style.css' : 'public/src/assets/sass/main.sass'
+        }
+      }
+    },
+
+    autoprefixer: {
+      dist: {
+        files: {
+          'public/dist/styles/style.css' : 'public/dist/styles/style.css'
+        }
+      }
+    },
+
+    jshint: {
+      all: ['Gruntfile.js', 'index.js', 'app/**/*.js', 'public/src/app/**/*.js']
+    },
+
+    uglify: {
+      options: {
+        mangle: false // may or may not need this option depending on how we format angular modules
+      },
+      dist: {
+        files: {
+          'public/dist/js/libs.min.js': [jsLibs],
+          'public/dist/js/app.min.js': [jsApp],
+        }
+      }
+    },
+
+    targethtml: {
+      dist: {
+        files: {
+          'public/index.html': 'public/src/index.html'
+        }
+      },
+      dev: {
+        files: {
+          'public/index.html': 'public/src/index.html'
+        }
+      }
+    },
+
+    watch: {
+      css: {
+        files: 'public/src/assets/sass/**/*.sass',
+        tasks: ['sass'],
+        options: {
+          livereload: true
+        }
+      },
+      jshint: {
+        files: ['index.js', 'app/**/*.js', 'public/src/app/**/*.js'],
+        tasks: ['jshint']
+      },
+      targethtml: {
+        files: 'public/src/index.html',
+        tasks: ['targethtml:dev']
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-sass');
@@ -15,5 +81,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-targethtml');
 
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['targethtml:dev', 'sass', 'jshint', 'watch']);
+  grunt.registerTask('build', ['targethtml:dist', 'sass', 'autoprefixer', 'uglify']);
 };
