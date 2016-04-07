cb48afbfe0579891a3d965e44d12e4d1da294ea3
David Nguyen <davidnguyen21@gmail.com>
2016-04-08 05:31:47 +0900

    basic server and static server up

--- app/server.js
+++ app/server.js
@@ -1,1 +1,8 @@
-// 
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/../public'));
+
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+var app = require('./app/server.js');
+
+
+var port = process.env.PORT || 8000;
+
+
+app.listen(port, function(){
+console.log("Activate interlock! Dynotherms connected! Infracells up! Mega thrusters are go! LET'S GO VOLTRON FORCE!");
+})
+
+
+
 // MAIN ENTRY POINT FOR THE APP
 
 // require in server modules from ./app/
 // fire up the server
+
+
+//wateva you say ben
