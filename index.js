be596e455b537eaee9a3af0a61ab2f0caafaf76d
Ben Janes <brjanes@gmail.com>
2016-04-07 17:28:59 -0400

    (chore) add semicolon to index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 8000;
 
 app.listen(port, function(){
 console.log("Activate interlock! Dynotherms connected! Infracells up! Mega thrusters are go! LET'S GO VOLTRON FORCE!");
-})
+});
 
 
 
