603b2f838c2f10c66ca3907a9fa6dcb40e9df8e5
Ben Janes <brjanes@gmail.com>
2016-04-08 08:45:46 -0400

    update dropdown nav

--- public/src/app/components/navigation/nav.js
+++ public/src/app/components/navigation/nav.js
@@ -2,15 +2,12 @@ angular.module('karaoke.nav', [])
 
 .controller('navCtrl', function($scope) {
   $scope.droppedDown = false;
-  // var notDroppedDown = true;
 
   $scope.toggleClass = function() {
-    // if (!droppedDown) {
-    //   console.log('not dropped down');
-    // } else {
-    //   console.log('dropped down');
-    // }
     $scope.droppedDown = !$scope.droppedDown;
   };
 
+  $scope.$on('$locationChangeStart', function(next, current) {
+    $scope.droppedDown = false;
+  });
 });
