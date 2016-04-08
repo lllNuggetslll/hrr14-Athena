8eba3e7b97fce58171f41e98e27b89c88b5537d8
Ben Janes <brjanes@gmail.com>
2016-04-08 08:44:09 -0400

    add auth components to app and to routing

--- public/src/app/app.js
+++ public/src/app/app.js
@@ -5,6 +5,7 @@ angular.module('karaoke', [
   'karaoke.services',
   'karaoke.nav',
   'karaoke.home',
+  'karaoke.auth',
   'ui.router'
 ])
 
@@ -16,5 +17,15 @@ angular.module('karaoke', [
       url: '/',
       templateUrl: 'src/app/components/home/home.html',
       controller: 'homeCtrl'
+    })
+    .state('signup', {
+      url: '/signup',
+      templateUrl: 'src/app/components/auth/signup.html',
+      controller: 'authCtrl'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'src/app/components/auth/login.html',
+      controller: 'authCtrl'
     });
 });
