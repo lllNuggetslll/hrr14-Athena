5eca85d9131bac1d13ffea98cedd6b6210515f51
Ben Janes <brjanes@gmail.com>
2016-04-08 10:22:55 -0400

    add location service to home controller

--- public/src/app/components/home/home.js
+++ public/src/app/components/home/home.js
@@ -1,7 +1,22 @@
 angular.module('karaoke.home', [])
 
-.controller('homeCtrl', function($scope, mapFactory) {
+.controller('homeCtrl', function($scope, $rootScope, locationFactory, mapFactory) {
 
-  $scope.title = 'HOME MAP';
+  $scope.lat = '';
+  $scope.long = '';
+
+  if (!$rootScope.userLocation) {
+    locationFactory.getPosition()
+    .then(function(pos) {
+      $rootScope.userLocation = pos.coords;
+      $scope.lat = pos.coords.latitude;
+      $scope.long = pos.coords.longitude;
+    });
+  } else {
+    $scope.lat = $rootScope.userLocation.latitude;
+    $scope.long = $rootScope.userLocation.longitude;
+  }
+  
+  // make a map
 
 });
