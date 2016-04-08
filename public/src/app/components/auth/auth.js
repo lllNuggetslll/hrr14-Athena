bce128a64fd823b1747b2cae0a53481d789f791e
Ben Janes <brjanes@gmail.com>
2016-04-08 09:10:26 -0400

    add login and signup forms

--- public/src/app/components/auth/auth.js
+++ public/src/app/components/auth/auth.js
@@ -2,7 +2,24 @@ angular.module('karaoke.auth', [])
 
 .controller('authCtrl', function($scope, mapFactory) {
 
-  $scope.signup = 'SIGNUP';
-  $scope.login = 'LOGIN';
+  $scope.user = {};
+
+  $scope.signup = function(isValid) {
+    if (isValid) {
+      // send user info to server
+      // if successful, tell user, start a session. set any info on $rootScope?
+      // else, indicate the error (user name already taken, etc.)
+      console.log('can submit');
+    }
+  };
+
+  $scope.login = function(isValid) {
+    if (isValid) {
+      // send user info to server
+      // if successful, start a session
+      // else, indicate the error (un does not exist, wrong password, etc.)
+      console.log('can submit');
+    }
+  };
 
 });
