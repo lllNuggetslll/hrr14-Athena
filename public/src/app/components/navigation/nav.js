db23790dcb2bd68fe4188b305bc2644afb5f4935
Ben Janes <brjanes@gmail.com>
2016-04-12 11:14:00 -0400

    add authfactory and logout method to nav controller

--- public/src/app/components/navigation/nav.js
+++ public/src/app/components/navigation/nav.js
@@ -1,10 +1,14 @@
 angular.module('karaoke.nav', [])
 
-.controller('navCtrl', function($scope) {
+.controller('navCtrl', function($scope, authFactory) {
   $scope.droppedDown = false;
 
   $scope.toggleClass = function() {
     $scope.droppedDown = !$scope.droppedDown;
+  };
+
+  $scope.logOut = function() {
+  	authFactory.logout();
   };
 
   $scope.$on('$locationChangeStart', function(next, current) {
