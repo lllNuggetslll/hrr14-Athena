7c6442ab8c81d46d706659dbb34c9b64c64e65a7
Ben Janes <brjanes@gmail.com>
2016-04-12 13:56:39 -0400

    add animation to menu icon

--- public/src/app/components/navigation/nav.js
+++ public/src/app/components/navigation/nav.js
@@ -4,8 +4,26 @@ angular.module('karaoke.nav', [])
   $scope.droppedDown = false;
   $scope.loggedIn = authFactory.isAuth();
 
+  var burgerLine1 = document.getElementById('burgerLine1');
+  var burgerLine2 = document.getElementById('burgerLine2');
+  var burgerLine3 = document.getElementById('burgerLine3');
+
   $scope.toggleClass = function() {
     $scope.droppedDown = !$scope.droppedDown;
+
+    if ($scope.droppedDown) {
+      Velocity(burgerLine1, { y1: '+= 6', y2: '+= 6' }, { duration: 400 });
+      Velocity(burgerLine1, { y1: '+= 7', y2: '-= 7'}, { duration: 400 });
+      Velocity(burgerLine2, { strokeOpacity: '0' }, { duration: 400 });
+      Velocity(burgerLine3, { y1: '-= 6', y2: '-= 6' }, { duration: 400 });
+      Velocity(burgerLine3, { y1: '-= 7', y2: '+= 7'}, { duration: 400 });
+    } else {
+      Velocity(burgerLine1, { y1: '-= 7', y2: '+= 7' }, { duration: 400 });
+      Velocity(burgerLine1, { y1: '-= 6', y2: '-= 6'}, { duration: 400 });
+      Velocity(burgerLine2, { strokeOpacity: '1' }, { duration: 400, delay: 400 });
+      Velocity(burgerLine3, { y1: '+= 7', y2: '-= 7' }, { duration: 400 });
+      Velocity(burgerLine3, { y1: '+= 6', y2: '+= 6'}, { duration: 400 });
+    }
   };
 
   $scope.logOut = function() {
