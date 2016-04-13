62de8948864a51a7ea3ab5533fb62afe2c23b2d0
Ben Janes <brjanes@gmail.com>
2016-04-13 12:06:16 -0400

    parse timestamp and show in event detail view

--- public/src/app/components/event/event.js
+++ public/src/app/components/event/event.js
@@ -10,10 +10,48 @@ angular.module('karaoke.event', [])
 
   eventFactory.getOne($stateParams.eventID)
   .then(function(response) {
+    var date = parseTime(response.time);
     $scope.creator = response.user.username;
     $scope.song = response.song_title;
-    $scope.artist = response.as_sung_by;
-    
+    $scope.artist = response.as_sung_by;    
+    $scope.date = date.day;
+    $scope.time = date.time;
   });
+
+  function parseTime(timestamp) {
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var date = new Date(timestamp);
+
+    var meridian = 'AM';
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+
+    if (hours === 0) {
+      hours = 12;
+    } else if (hours === 12) {
+      meridian = 'PM';
+    } else if (hours > 12) {
+      hours = hours - 12;
+      meridian = 'PM';
+    }
+
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+
+    var day = weekdays[date.getDay()];
+    day += ', ' + months[date.getMonth()];
+    day += ' ' + date.getDate();
+
+    var time = '' + hours;
+    time += ':' + minutes;
+    time += ' ' + meridian;
+
+    return {
+      day : day,
+      time : time
+    };
+  }
 
 });
