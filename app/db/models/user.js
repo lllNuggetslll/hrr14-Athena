ee1d4e0acb602170b37384ce9ea745a7a580a5f6
David Nguyen <davidnguyen21@gmail.com>
2016-04-14 01:32:52 +0900

    added relational id's and event collection

--- app/db/models/user.js
+++ app/db/models/user.js
@@ -10,7 +10,7 @@ var User = Bookshelf.Model.extend({
   tableName: 'users',
 
   events: function() {
-    return this.hasMany('Event');
+    return this.hasMany('Event', "events_id");
   },
 
   comparePassword: function(attemptedPassword, callback) {
